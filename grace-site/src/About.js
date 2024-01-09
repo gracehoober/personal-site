c95005a26c2e49830ebc827b5d336f2579d71e63
Grace Bethany Hoober <grace.hoober66@gmail.com>
2024-01-08 16:58:51 -0800

    added portrait and title to About component

--- grace-site/src/About.js
+++ grace-site/src/About.js
@@ -1,6 +1,16 @@
-
+import portrait from "./images/portrait.jpg"
+import "./About.css"
+/**
+ *
+ */
 
 function About() {
-
+  return (
+    <div className="container1">
+      <h1>hihi!</h1>
+      <img className="portrait" src={portrait} alt="portrait-grace"></img>
+    </div>
+  );
 }
+
 export default About;
