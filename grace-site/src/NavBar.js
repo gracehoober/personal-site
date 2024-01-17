3a096f469024869cb593e5df61610178b8af5b80
Grace Bethany Hoober <grace.hoober66@gmail.com>
2024-01-17 15:38:10 -0800

    navbar links centered

--- grace-site/src/NavBar.js
+++ grace-site/src/NavBar.js
@@ -7,12 +7,12 @@ import { NavLink } from 'react-router-dom';
  */
 function NavBar() {
   return (
-    <nav className="flex flex-row fixed top-0 w-screen h-16 text-center text-forestgreen bg-sage drop-shadow-xl">
+    <nav className="flex flex-row justify-around items-center fixed top-0 w-screen h-16 text-center text-forestgreen text-xl bg-sage drop-shadow-xl">
       <h1 className="text-green-700 ">Grace Hoober</h1>
-      <NavLink className="text-green-700 focus:text-redbrown
+      <NavLink className="text-green-700 focus:text-navy
       hover:text-redbrown" to="/home">About</NavLink>
-      <NavLink className="focus:text-redbrown hover:text-redbrown" to="/projects">Projects</NavLink>
-      <NavLink className="focus:text-redbrown hover:text-redbrown" to="/contact">Contact</NavLink>
+      <NavLink className="focus:text-navy hover:text-navy" to="/projects">Projects</NavLink>
+      <NavLink className="focus:text-navy hover:text-navy" to="/contact">Contact</NavLink>
     </nav>
   );
 }
